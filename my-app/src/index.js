555c796ce9fe0a5df32dbe45e71d5769267d6605
Leah-Mei Villanueva <leahmei@hawaii.edu>
2018-10-09 21:30:43 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class Nike extends React.Component {
 
   render() {
     return (
-        <h1>Nike!</h1>
-  );
+        <Container textAlign="center">
+          <Header as='h1'>Nike!</Header>
+        </Container>
+    );
   }
 }
 
-ReactDOM.render(<Nike />, document.getElementById('root'));
+ReactDOM.render(<Nike/>, document.getElementById('root'));
